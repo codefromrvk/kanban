079e29aaccf83c4e0e5b5b7a4cc3eef6d08ccf45
R Varshith Kumar <72021268+codefromrvk@users.noreply.github.com>
2022-07-12 14:06:51 +0530

    Update axios.js

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://quiet-hollows-05652.herokuapp.com/"||"http://localhost:5000",
 });
